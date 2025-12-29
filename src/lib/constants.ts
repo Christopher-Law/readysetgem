ac218e0d9f245dd682bf8ccf4e52d2bfa935d5b7
Christopher-Law <christopher-law@live.com>
2025-12-29 09:40:04 -0500

    update copy about card restoration

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,5 +8,5 @@ export const socials = [
 ] as const;
 
 export const mailtoHref =
-  "mailto:chris@slabworks.ca?subject=Card%20Restoration%20Request&body=Hi%20Slabworks%2C%0A%0AI'd%20like%20to%20request%20restoration%20for%20some%20cards%3A%0A%0A-%20Name%3A%0A-%20Total%20number%20of%20cards%20to%20be%20repaired%3A%0A-%20Card%20names%20%2B%20set%20they%20are%20from%3A%0A-%20Suspected%20damage%3A%0A-%20Goal%20(display%2Fgrading%2Fcollection%20etc)%3A%0A-%20Photos%20of%20cards%3A%0A%0AThanks!";
+  "mailto:chris@slabworks.ca?subject=Trading%20Card%20Restoration%20Request&body=Hi%20Slabworks%2C%0A%0AI'd%20like%20to%20request%20restoration%20for%20some%20cards%3A%0A%0A-%20Name%3A%0A-%20Total%20number%20of%20cards%20to%20be%20repaired%3A%0A-%20Card%20names%20%2B%20set%20they%20are%20from%3A%0A-%20Suspected%20damage%3A%0A-%20Goal%20(display%2Fgrading%2Fcollection%20etc)%3A%0A-%20Photos%20of%20cards%3A%0A%0AThanks!";
 
